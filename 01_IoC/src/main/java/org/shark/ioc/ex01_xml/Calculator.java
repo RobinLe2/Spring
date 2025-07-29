5fc394e2c447caaf105f661e30bb7c34376d216c
teacher-min <minkt78@gmail.com>
2025-07-29 15:54:25 +0900

    20250729

---
+++ 01_IoC/src/main/java/org/shark/ioc/ex01_xml/Calculator.java
@@ -0,0 +1,21 @@
+package org.shark.ioc.ex01_xml;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor  //----- Calculator() : 디폴트 생성자
+@AllArgsConstructor  //---- Calculator(String, int, Adder, Subtractor, Multiplier, Divider)
+@Getter
+@Setter
+@ToString
+public class Calculator {
+  private String brand;
+  private int price;
+  private Adder adder;
+  private Subtractor subtractor;
+  private Multiplier multiplier;
+  private Divider divider;
+}
