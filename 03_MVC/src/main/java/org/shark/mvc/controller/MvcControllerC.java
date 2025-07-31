cac4b63d0849824eced5d6e6e0f006eab151ba64
teacher-min <minkt78@gmail.com>
2025-07-31 10:01:22 +0900

    20250731

--- 03_MVC/src/main/java/org/shark/mvc/controller/MvcControllerC.java
+++ 03_MVC/src/main/java/org/shark/mvc/controller/MvcControllerC.java
@@ -13,13 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MvcControllerC {
 
-  //----- List<BoardDTO> boards 반환
-  private List<BoardDTO> getBoards() {
-    List<BoardDTO> boards = new ArrayList<BoardDTO>();
+  List<BoardDTO> boards = new ArrayList<BoardDTO>();
+
+  public MvcControllerC() {
     boards.add(new BoardDTO("제목1", 20));
     boards.add(new BoardDTO("제목2", 10));
     boards.add(new BoardDTO("제목3", 30));
-    return boards;
   }
   
   //----- ModelAndView를 이용한 forward (데이터 전달)
@@ -27,7 +26,7 @@ public class MvcControllerC {
   public ModelAndView methodA() {
     ModelAndView mv = new ModelAndView();
     mv.setViewName("c/list");
-    mv.addObject("boards", getBoards());  // JSP로 전달되는 데이터 boards
+    mv.addObject("boards", boards);  // JSP로 전달되는 데이터 boards
     return mv;
   }
   
@@ -35,7 +34,7 @@ public class MvcControllerC {
   @RequestMapping("/detail")  //----------- 요청 주소 : /c/detail?bid=1
   public String methodB(Model model  //---- JSP로 전달할 데이터를 저장할 Model
                       , int bid) {  //----- @RequestParam 생략
-    model.addAttribute("board", getBoards().get(bid));  //-- JSP로 전달되는 데이터 board
+    model.addAttribute("board", boards.get(bid));  //-- JSP로 전달되는 데이터 board
     return "c/detail";
   }
 
